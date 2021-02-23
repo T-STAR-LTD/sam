ffcdb7b7483404ce1bbdc8256377e25705be158b
Takayuki Hoshino <takayuki.hoshino.co.ltd@gmail.com>
2021-02-23 11:52:31 +0900

    Colab TPU support test

--- sam_jax/train.py
+++ sam_jax/train.py
@@ -29,6 +29,16 @@ from sam.sam_jax.training_utils import flax_training
 import tensorflow.compat.v2 as tf
 from tensorflow.io import gfile
 
+import requests
+import os
+if 'TPU_DRIVER_MODE' not in globals():
+    url = 'http://' + os.environ['COLAB_TPU_ADDR'].split(':')[0] + ':8475/requestversion/tpu_driver0.1-dev20191206'
+    resp = requests.post(url)
+
+from jax.config import config
+config.FLAGS.jax_xla_backend = "tpu_driver"
+config.FLAGS.jax_backend_target = "grpc://" + os.environ['COLAB_TPU_ADDR']
+print(config.FLAGS.jax_backend_target)
 
 FLAGS = flags.FLAGS
 
